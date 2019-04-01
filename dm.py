dbdd0b01b89cca84729ae1413143a08400b0c4f9
GokulVSD <gokul.vasda@gmail.com>
2019-04-01 23:45:29 +0530

    Minor fix

--- dm.py
+++ dm.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import math
 
 raw_data = pd.read_csv('inputs/trending.csv')
 raw_categories = pd.read_json('inputs/categories.json')
@@ -14,13 +15,22 @@ data = {}
 labels = {}
 count = 0
 total = len(raw_data.values)
+
 for i in raw_data.values:
+
     count += 1
     print("Preprocessing Data: ", int((count/total)*100), "%", end='\r')
+
     if i[0] not in data.keys():
+
         title_caps_ratio = sum(1 for c in i[2] if c.isupper()) / len(i[2])
         no_of_tags = sum(1 for c in i[6] if c == '|') + 1
-        desc_caps_ratio = sum(1 for c in i[15] if c.isupper()) / len(i[15])
+
+        try:
+            desc_caps_ratio = sum(1 for c in i[15] if c.isupper()) / len(i[15])
+        except:
+            desc_caps_ratio = -1
+
         data[i[0]] = {
             'title_caps_ratio': title_caps_ratio,
             'title_len': len(i[2]),
@@ -31,10 +41,12 @@ for i in raw_data.values:
             'no_of_comments_first_day': i[10],
             'description_caps_ratio': desc_caps_ratio
         }
+
         labels[i[0]] = {'days_trending': len(
             raw_data[raw_data.video_id == i[0]].values)}
         if count == 20:
             break #remove later
+
 
 for i, j in data.items():
     print(j.keys())
